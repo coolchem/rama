8e33a88fe9045e664655a66f329f466f903bb677
coolchem <varun8686@gmail.com>
2016-01-31 18:01:43 -0700

    fixing CI build

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="main/ambient/jasmine/jasmine.d.ts" />
 /// <reference path="main/ambient/node/node.d.ts" />
-/// <reference path="main/ambient/webcomponents.js/webcomponents.js.d.ts" />
