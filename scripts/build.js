2a0900cc6ed923714e0e3ab8979d2f4c02ec2ea4
coolchem <varun8686@gmail.com>
2016-07-11 14:41:50 -0600

    creating stand alone file -- will be committed every new release

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,8 @@
 var util = require('./util');
 var argv = require('minimist')(process.argv.slice(2));
 var cpy = require("cpy");
+var path = require("path");
+var fs = require("fs-extra");
 
 
 if(argv._ && argv._.length > 0) //look release build
@@ -17,6 +19,48 @@ if(argv._ && argv._.length > 0) //look release build
 else //do dev build
 {
     build();
+}
+
+function bundleFiles(cb){
+
+    var stealTools = require("steal-tools");
+
+    stealTools.build({
+            main: "src/index",
+            bundlesPath:path.resolve("./tmp"),
+            config: path.resolve("./")+"/package.json!npm"
+        },
+        {
+            minify: true,
+            debug: false,
+            bundleSteal: true
+        }
+    ).then(function(){
+
+        json = JSON.parse(fs.readFileSync(path.resolve("./package.json"), 'utf8'));
+        var version = json.version;
+
+        try {
+            fs.lstatSync(path.resolve("./standalone/rama.js"));
+            fs.copySync(path.resolve("./standalone/rama.js"), path.resolve("./standalone") + "/rama"+version+".js");fs.removeSync(process.cwd()+"/myProject");
+        }
+        catch (e) {
+            //continue
+        }
+        
+        fs.copy(path.resolve("./tmp/src/index.js"), path.resolve("./standalone")+"/rama.js", function (err) {
+            if (err) 
+                return cb(err);
+            else 
+            {
+                fs.removeSync(path.resolve("./tmp"));
+                cb()
+            }
+        })
+
+    },function(err){
+        cb(err)
+    })
 }
 
 function build(isRelease){
@@ -40,8 +84,19 @@ function build(isRelease){
             if(isRelease)
             {
                 cpy(["**/*.js","**/*.d.ts"],"../dist",{cwd:process.cwd()+"/src",parents: true, nodir: true}).then(function(){
-
-                    process.exit(0);
+                    
+                    bundleFiles(function (err) {
+                        
+                        if(err)
+                        {
+                            console.log(err);
+                            process.exit(1);
+                        }
+                        else 
+                        {
+                            process.exit(0);
+                        }
+                    })
 
                 },function(err){
 
